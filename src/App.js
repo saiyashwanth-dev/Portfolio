0351142f5a0e42741bc2386787aa56a494a3d8f1
saiyashwanth-dev <yashwant.b13@gmail.com>
2024-11-16 13:05:13 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import StartCanvas from "./components/canvas/Stars";
 import Projects from "./components/sections/Projects";
 import Contact from "./components/sections/Contact";
 import Footer from "./components/sections/Footer";
-
+//code from here
 const Body = styled.div`
   background-color: ${({ theme }) => theme.bg};
   width: 100%;
